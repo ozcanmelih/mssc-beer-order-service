044289668d0ed21952fb703e7d79d08464f23b2c
ozcanmelih <melih.ozcan89@gmail.com>
2020-10-25 19:09:58 +0300

    allocate order

---
+++ src/main/java/guru/sfg/beer/order/service/services/listeners/BeerOrderValidationResultListener.java
@@ -0,0 +1,30 @@
+package guru.sfg.beer.order.service.services.listeners;
+
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.beer.order.service.services.BeerOrderManager;
+import guru.sfg.brewery.model.events.ValidateOrderResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerOrderValidationResultListener {
+
+    private final BeerOrderManager beerOrderManager;
+
+    @Transactional
+    @JmsListener(destination = JmsConfig.VALIDATE_ORDER_RESPONSE_QUEUE)
+    public void listen(ValidateOrderResult validateOrderResult) {
+        UUID beerOrderId = validateOrderResult.getOrderId();
+
+        log.debug("Validation Result for Order Id: " + beerOrderId);
+
+        beerOrderManager.processValidationResult(beerOrderId, validateOrderResult.getIsValid());
+    }
+}
